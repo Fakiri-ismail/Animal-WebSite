af76a29a27d8337ad21decd9b7d4a6891c299b43
Fakiri ismail <ismaiilfakirii@gmail.com>
2021-04-24 23:26:46 +0000

    add animal_details function

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 animals = [
-    {'Name': 'Nelly','Age': '5 weeks','Bio': 'I am a tiny kitten rescued by the good people at Paws Rescue Center.'},
-    {'Name': 'Yuki','Age': '3 weeks','Bio': 'I am a handsome gentle-cat. I like to dressup in bow ties.'},
-    {'Name': 'Basker','Age': '1 year','Bio': 'I love barking. But, I love my friends more.'}
+    {'id':1, 'Name': 'Nelly', 'Age': '5 weeks', 'Bio': 'I am a tiny kitten rescued by the good people.'},
+    {'id':2, 'Name': 'Yuki', 'Age': '3 weeks', 'Bio': 'I am a handsome gentle-cat. I like to dressup in bow ties.'},
+    {'id':3, 'Name': 'Basker', 'Age': '1 year', 'Bio': 'I love barking. But, I love my friends more.'}
         ]
 
 @app.route("/")
@@ -20,9 +20,12 @@ def about():
 
 
 # Dynamic routing
-@app.route('/<int:number>')  # String (default), int, float, path (Strings with slashes), uuid.
-def show_square(number):
-    return "Square of " + str(number) + " is: " + str(number * number)
+@app.route("/details/<int:animal_id>")     # String (default), int, float, path (Strings with slashes), uuid.
+def animal_details(animal_id):
+    animal = next((animal for animal in animals if animal["id"] == animal_id), None)
+    if animal is None:
+        abort(404, description="No animal was Found with the given ID")
+    return render_template("details.html", animal=animal)
 
 
 if __name__ == "__main__":
