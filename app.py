4d432318ba0b39873c7ce2dea8645e4e974ca727
Fakiri ismail <ismaiilfakirii@gmail.com>
2021-04-24 16:51:05 +0000

    for Loop in Jinja

--- app.py
+++ app.py
@@ -2,22 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+animals = [
+    {'Name': 'Nelly','Age': '5 weeks','Bio': 'I am a tiny kitten rescued by the good people at Paws Rescue Center.'},
+    {'Name': 'Yuki','Age': '3 weeks','Bio': 'I am a handsome gentle-cat. I like to dressup in bow ties.'},
+    {'Name': 'Basker','Age': '1 year','Bio': 'I love barking. But, I love my friends more.'}
+        ]
 
 @app.route("/")
 def home():
-    animals = [{'Image':'1.jpg',
-                'Name': 'Nelly',
-                'Age': '5 weeks',
-                'Bio': 'I am a tiny kitten rescued by the good people at Paws Rescue Center.'},
-               {'Image':'2.jpg',
-                'Name': 'Yuki',
-                'Age': '3 weeks',
-                'Bio': 'I am a handsome gentle-cat. I like to dressup in bow ties.'},
-               {'Image':'3.jpg',
-                'Name': 'Basker',
-                'Age': '1 year',
-                'Bio': 'I love barking. But, I love my friends more.'}
-               ]
     return render_template("home.html", animals=animals)
 
 
