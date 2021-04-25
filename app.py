6e5494d20dd2db6af80936aafc27d8b403576897
Fakiri ismail <ismaiilfakirii@gmail.com>
2021-04-25 16:49:30 +0000

    add requirement file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, abort, request
-from forms import LoginForm
+from flask import session, redirect, url_for
+from forms import LoginForm, SingUpForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
@@ -8,9 +9,11 @@ animals = [
     {'id':1, 'Name': 'Nelly', 'Age': '5 weeks', 'Bio': 'I am a tiny kitten rescued by the good people.'},
     {'id':2, 'Name': 'Yuki', 'Age': '3 weeks', 'Bio': 'I am a handsome gentle-cat. I like to dressup in bow ties.'},
     {'id':3, 'Name': 'Basker', 'Age': '1 year', 'Bio': 'I love barking. But, I love my friends more.'}
-        ]
+          ]
 
-users = {"fakiri@gmail.com": "123456", "ismail@email.com": "azerty"}
+users = [
+    {"id": 1, "fullName": "Fakiri Ismail", "email": "fakiri@gmail.com", "password": "123456"},
+        ]
 
 @app.route("/")
 def home():
@@ -21,9 +24,10 @@ def login():
     form = LoginForm()
     # form.is_submitted()    form.validate()    form.validate_on_submit()
     if form.validate_on_submit():
-        for u_email, u_password in users.items():
-            if u_email == form.email.data and u_password == form.password.data:
-                return render_template("login.html", form=form, message="Successfully Logged In")
+        for user in users:
+            if user["email"] == form.email.data and user['password'] == form.password.data:
+                session['user'] = user
+                return render_template("home.html", animals=animals)
         return render_template("login.html", form=form, message="Incorrect Email or Password")
     elif form.errors:
         print(form.errors.items())  # return dict()
@@ -31,6 +35,31 @@ def login():
         print(form.password.errors) # return list()
 
     return render_template("login.html", form=form)
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('login')) # more parameter :  _scheme='https', _external=True
+
+@app.route("/singUp", methods=["GET", "POST"])
+def singUp():
+    form = SingUpForm()
+    user = {}
+    if form.validate_on_submit():
+        user["id"] = len(users)+1
+        user["fullName"] = form.fullName.data
+        user["email"] = form.email.data
+        user["password"] = form.password1.data
+        users.append(user)
+        loginForm=LoginForm()
+        return render_template("login.html", form=loginForm)
+    elif form.errors:
+        print(form.errors.items())
+        print(form.email.errors[0])
+        print(form.password2.errors[0])
+
+    return render_template("singup.html", form=form)
 
 # Static routing
 @app.route("/about")
