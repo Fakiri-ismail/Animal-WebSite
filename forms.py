582a9ec48714600af31f74d0a1b69c7bcf4654c9
Fakiri ismail <ismaiilfakirii@gmail.com>
2021-04-25 16:54:30 +0000

    SingUp form

--- forms.py
+++ forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, EqualTo
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
+
+class SingUpForm(FlaskForm):
+    fullName = StringField('Full Name', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password1 = PasswordField('Password', validators=[InputRequired()])
+    password2 = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password1', message='Passwords must match')])
+    submit = SubmitField('SingUP')
