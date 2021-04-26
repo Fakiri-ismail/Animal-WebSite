7bd49460f34102730da995e7a7d9ee33e4abc874
Fakiri ismail <ismaiilfakirii@gmail.com>
2021-04-26 03:01:33 +0000

    Edit Animal Form

--- forms.py
+++ forms.py
@@ -2,10 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email, EqualTo
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
+
 
 class SingUpForm(FlaskForm):
     fullName = StringField('Full Name', validators=[InputRequired()])
@@ -13,3 +15,10 @@ class SingUpForm(FlaskForm):
     password1 = PasswordField('Password', validators=[InputRequired()])
     password2 = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password1', message='Passwords must match')])
     submit = SubmitField('SingUP')
+
+
+class EditAnimalForm(FlaskForm):
+    name = StringField("Animal's Name", validators=[InputRequired()])
+    age = StringField("Animal's Age", validators=[InputRequired()])
+    bio = StringField("Animal's Bio", validators=[InputRequired()])
+    submit = SubmitField("Edit Animal")
